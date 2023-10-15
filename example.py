b87318434b0fe0d99d0f06b5dc0d7b89cd5e393c
Gits0L <solinere@outlook.com>
2023-10-16 00:54:25 +0100

    line 1 change

--- example.py
+++ example.py
@@ -1,6 +1,1 @@
-import time
-
-# This is an example python script to test merge conflicts
-time.sleep(1)
-print(f"Hello World!, the time is {time.time()}")
-time.sleep(1)
+# This line in main will create a merge conflict
